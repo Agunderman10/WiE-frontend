d7555e75be699c929f44dfff8ba55064c85fafbb
agunderman10 <agunderman10@gmail.com>
2020-09-03 08:47:48 -0400

    Moved Screens and navigator into their own components

--- App.js
+++ App.js
@@ -1,72 +1,12 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import { Home } from './src/screens/Home/index';
-import { Resources } from './src/screens/Resources/index';
-import { StudyGroups } from './src/screens/StudyGroups/index';
-import { FAQ } from './src/screens/FAQ/index';
-
-const Tab = createBottomTabNavigator();
-
-function MyTabs() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      tabBarOptions={{
-        activeTintColor: '#e91e63',
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{
-          tabBarLabel: 'Home',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Resources"
-        component={Resources}
-        options={{
-          tabBarLabel: 'Resources',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="bell" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Study Groups"
-        component={StudyGroups}
-        options={{
-          tabBarLabel: 'Study Groups',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="FAQ"
-        component={FAQ}
-        options={{
-          tabBarLabel: 'FAQ',
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
+import { Screens } from './src/screens/index';
 
 export default function App() {
   return (
     <NavigationContainer>
-      <MyTabs />
+      <Screens />
     </NavigationContainer>
   );
 }
---
+++ src/screens/index.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { Home } from './Home/index';
+import { Resources } from './Resources/index';
+import { StudyGroups } from './StudyGroups/index';
+import { FAQ } from './FAQ/index';
+
+const Tab = createBottomTabNavigator();
+
+export function Screens() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: '#e91e63',
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Resources"
+        component={Resources}
+        options={{
+          tabBarLabel: 'Resources',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="bell" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Study Groups"
+        component={StudyGroups}
+        options={{
+          tabBarLabel: 'Study Groups',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="FAQ"
+        component={FAQ}
+        options={{
+          tabBarLabel: 'FAQ',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
