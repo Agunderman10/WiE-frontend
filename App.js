35d96b86a364cb802309efcea3d9017755556962
agunderman10 <agunderman10@gmail.com>
2020-09-03 14:30:34 -0400

    Added landing page structure for checking if user is logged in

--- App.js
+++ App.js
@@ -2,11 +2,21 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
 import { Screens } from './src/screens/index';
+import { Landing } from './src/screens/Landing/index';
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Screens />
-    </NavigationContainer>
-  );
+  const loggedin = false;
+
+  if(loggedin === true) {
+    return (
+      <NavigationContainer>
+        <Screens />
+      </NavigationContainer>
+    );
+  }
+  else {
+    return (
+      <Landing />
+    );
+  }
 }
---
+++ src/screens/Landing/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const Landing = () => {
+  return (
+    <View>
+      <Text>Landing Page</Text>
+    </View>
+  );
+};
