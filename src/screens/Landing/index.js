b9432d4659801041b2c5547ecefeb5cbd1683177
agunderman10 <agunderman10@gmail.com>
2020-09-04 10:39:25 -0400

    Added login page structure

--- src/screens/Landing/index.js
+++ src/screens/Landing/index.js
@@ -1,5 +1,14 @@
 import React, { useState } from "react";
-import { Text, View, TextInput, TouchableOpacity, Modal } from "react-native";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  Modal,
+  Image,
+  OpaqueColorValue,
+  ImageBackground,
+} from "react-native";
 
 import { styles } from "./styles";
 
@@ -14,17 +23,25 @@ export const Landing = () => {
   const [modalVisible, setModalVisible] = useState(true);
   return (
     <Modal visible={modalVisible} animationType="slide">
-      <View style={styles.container}>
-        <Text>Landing Page</Text>
-        <TextInput placeholder="OSU Email..." style={styles.textInput} />
-        <TextInput placeholder="Password..." style={styles.textInput} />
-        <TouchableOpacity onPress={signIn} style={styles.loginButton}>
-          <Text>Sign In</Text>
-        </TouchableOpacity>
-        <View style={styles.signUpLabel}>
-          <Text>Don't have an account? Sign up</Text>
+      <ImageBackground
+        style={styles.backgroundImage}
+        source={require("../../../images/Oval.jpg")}
+      >
+        <View style={styles.container}>
+          <Image
+            style={styles.frontImage}
+            source={{ uri: "../../../images/Oval.jpg" }}
+          />
+          <TextInput placeholder="OSU Email..." style={styles.textInput} />
+          <TextInput placeholder="Password..." style={styles.textInput} />
+          <TouchableOpacity onPress={signIn} style={styles.loginButton}>
+            <Text>Sign In</Text>
+          </TouchableOpacity>
+          <View style={styles.signUpLabel}>
+            <Text>Don't have an account? Sign up</Text>
+          </View>
         </View>
-      </View>
+      </ImageBackground>
     </Modal>
   );
 };
--- src/screens/Landing/styles.js
+++ src/screens/Landing/styles.js
@@ -4,6 +4,18 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
+        zIndex: 100,
+    },
+    backgroundImage: {
+        flex: 1,
+        resizeMode: 'cover',
+        zIndex: -1000,
+        width: '100%',
+        height: '100%',
+    },
+    frontImage: {
+        height: 200,
+        width: 200,
     },
     textInput: {
         borderRadius: 10,
@@ -11,6 +23,7 @@ export const styles = StyleSheet.create({
         width: 250,
         height: 40,
         marginBottom: 30,
+        backgroundColor: "white",
     },
     loginButton: {
         justifyContent: "center",
