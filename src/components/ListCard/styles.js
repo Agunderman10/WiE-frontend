14008e44dd89d0f4776b139cfef917839cbbeee5
agunderman10 <agunderman10@gmail.com>
2020-09-07 21:11:35 -0400

    Changed ListCard border to grey

--- src/components/ListCard/styles.js
+++ src/components/ListCard/styles.js
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 export const styles = StyleSheet.create({
     container: {
         backgroundColor: "white",
-        borderColor: "black",
+        borderColor: "lightgrey",
         borderWidth: 1,
         //marginLeft: '3%',
         marginTop: '3%',
