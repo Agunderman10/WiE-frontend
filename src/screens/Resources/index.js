ee8576080b8b073dcabf2add8c5cfc250064f01f
agunderman10 <agunderman10@gmail.com>
2020-09-07 21:28:17 -0400

    Added basic layout for Resources page

--- src/screens/Resources/index.js
+++ src/screens/Resources/index.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+
+import { Card } from "../../components/Card/index";
+import { Header } from "./../../components/Header/index";
+import { ListCard } from "./../../components/ListCard/index";
+import { styles } from "./styles";
 
 export function Resources() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Resources!</Text>
-      </View>
-    );
-  }
+  const DATA = [
+    {
+      image: "./../../../images/Oval.jpg",
+      label: "Summary",
+    },
+    {
+      image: "./../../../images/Oval.jpg",
+      label: "How to Join",
+    },
+    {
+      image: "./../../../images/Oval.jpg",
+      label: "Contact Info",
+    },
+  ];
+
+  const renderItem = ({ item }) => {
+    return <Card image={item.image} label={item.label} />;
+  };
+
+  return (
+    <View>
+    <Header title={"Resources"} />
+      <ListCard>
+        <Text style={styles.resourceTypeHeader}>ACE Tutoring</Text>
+        <FlatList
+          horizontal={true}
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.label}
+        />
+      </ListCard>
+
+      <ListCard>
+        <Text style={styles.resourceTypeHeader}>Drackett/Dorm Tutoring</Text>
+        <FlatList
+          horizontal={true}
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.label}
+        />
+      </ListCard>
+
+      <ListCard>
+        <Text style={styles.resourceTypeHeader}>MSLC</Text>
+        <FlatList
+          horizontal={true}
+          data={DATA}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.label}
+        />
+      </ListCard>
+    </View>
+  );
+}
--- src/screens/Resources/styles.js
+++ src/screens/Resources/styles.js
@@ -1,5 +1,13 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    
+    resourceTypeHeader: {
+        position: "relative",
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: "black",
+        marginLeft: '4%',
+        marginTop: '3%',
+        marginBottom: '1%',
+    },
 });
