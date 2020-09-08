ec93e26007471242f6331f55a0f0a6c4435742a0
agunderman10 <agunderman10@gmail.com>
2020-09-07 21:39:10 -0400

    Added StudyGroups initial design structure

--- src/screens/StudyGroups/index.js
+++ src/screens/StudyGroups/index.js
@@ -1,10 +1,75 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React from "react";
+import { View, Text, FlatList, ScrollView } from "react-native";
+
+import { Card } from "../../components/Card/index";
+import { Header } from "./../../components/Header/index";
+import { ListCard } from "./../../components/ListCard/index";
+import { styles } from "./styles";
 
 export function StudyGroups() {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Study Groups!</Text>
-      </View>
-    );
-  }
+  const DATA = [
+    {
+      image: "./../../../images/Oval.jpg",
+      label: "Monday Recitation 12:00pm",
+    },
+    {
+      image: "./../../../images/Oval.jpg",
+      label: "Wednesday Lecture 12:00pm",
+    },
+    {
+      image: "./../../../images/Oval.jpg",
+      label: "Friday Lecture 12:00pm",
+    },
+  ];
+
+  const renderItem = ({ item }) => {
+    return <Card image={item.image} label={item.label} />;
+  };
+
+  return (
+    <View>
+      <ScrollView>
+        <Header title={"Study Groups"} />
+        <ListCard>
+          <Text style={styles.groupHeader}>FE 1181</Text>
+          <FlatList
+            horizontal={true}
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.label}
+          />
+        </ListCard>
+
+        <ListCard>
+          <Text style={styles.groupHeader}>FEH</Text>
+          <FlatList
+            horizontal={true}
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.label}
+          />
+        </ListCard>
+
+        <ListCard>
+          <Text style={styles.groupHeader}>Physics 1250</Text>
+          <FlatList
+            horizontal={true}
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.label}
+          />
+        </ListCard>
+
+        <ListCard>
+          <Text style={styles.groupHeader}>Chem 1210</Text>
+          <FlatList
+            horizontal={true}
+            data={DATA}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.label}
+          />
+        </ListCard>
+      </ScrollView>
+    </View>
+  );
+}
--- src/screens/StudyGroups/styles.js
+++ src/screens/StudyGroups/styles.js
@@ -1,5 +1,13 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
+    groupHeader: {
+        position: "relative",
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: "black",
+        marginLeft: '4%',
+        marginTop: '3%',
+        marginBottom: '1%',
+    },
 });
