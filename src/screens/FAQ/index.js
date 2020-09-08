341bbccc90edecdd3ba50160e40e96b84a3d8903
agunderman10 <agunderman10@gmail.com>
2020-09-07 21:01:05 -0400

    Added scrolling to FAQ page

--- src/screens/FAQ/index.js
+++ src/screens/FAQ/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 
 import { Header } from "./../../components/Header/index";
 import { ListCard } from "./../../components/ListCard/index";
@@ -9,53 +9,71 @@ import { styles } from "./styles";
 export function FAQ() {
   return (
     <View>
-      <Header title={"FAQ"} />
-      <ListCard>
-        <Text>Discussion Board</Text>
-      </ListCard>
-      <ListCard>
-        <QuestionCard
-          questionBody={
-            "You will be living in Drackett with the rest of the learning community."
-          }
-          header={"What should I expect for housing my first year?"}
-        />
-      </ListCard>
+      <ScrollView>
+        <Header title={"FAQ"} />
+        <ListCard>
+          <Text>Discussion Board</Text>
+        </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "You will be living in Drackett with the rest of the learning community."
+            }
+            header={"What should I expect for housing my first year?"}
+          />
+        </ListCard>
 
-      <ListCard>
-        <QuestionCard
-          questionBody={
-            "You need to have at least 16 credit hours of classes that qualify for the program."
-          }
-          header={"What are the requirements for graduating in the program?"}
-        />
-      </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "You need to have at least 16 credit hours of classes that qualify for the program."
+            }
+            header={"What are the requirements for graduating in the program?"}
+          />
+        </ListCard>
 
-      <ListCard>
-        <QuestionCard
-          questionBody={
-            "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
-          }
-          header={"What does a typical first year schedule look like?"}
-        />
-      </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
+            }
+            header={"What does a typical first year schedule look like?"}
+          />
+        </ListCard>
 
-      <ListCard>
-        <QuestionCard
-          questionBody={
-            "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
-          }
-          header={"What does a typical first year schedule look like?"}
-        />
-      </ListCard>
-      <ListCard>
-        <QuestionCard
-          questionBody={
-            "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
-          }
-          header={"What does a typical first year schedule look like?"}
-        />
-      </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
+            }
+            header={"What does a typical first year schedule look like?"}
+          />
+        </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
+            }
+            header={"What does a typical first year schedule look like?"}
+          />
+        </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
+            }
+            header={"What does a typical first year schedule look like?"}
+          />
+        </ListCard>
+        <ListCard>
+          <QuestionCard
+            questionBody={
+              "It is typically around 16 credit hours. Over 18 will cost extra, so it's suggested to stay below that threshold."
+            }
+            header={"What does a typical first year schedule look like?"}
+          />
+        </ListCard>
+      </ScrollView>
     </View>
   );
 }
