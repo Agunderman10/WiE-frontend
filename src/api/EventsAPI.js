bbab41d2f627e00c29a892e973fc106e2be3c670
agunderman10 <agunderman10@gmail.com>
2020-09-15 16:15:34 -0400

    Added async when getting events

--- src/api/EventsAPI.js
+++ src/api/EventsAPI.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { url } from "./../constants/app";
 
-export const getEvents = () => {
-  return axios
+export const getEvents = async () => {
+  return await axios
     .get(url + "events")
     .then((res) => {
       return res.data;
