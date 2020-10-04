4f43abf736420ebd1689190e0705207d332e0a28
agunderman10 <agunderman10@gmail.com>
2020-10-04 11:57:37 -0400

    Removed unused import

--- App.js
+++ App.js
@@ -3,7 +3,6 @@ import { NavigationContainer } from "@react-navigation/native";
 
 import { Screens } from "./src/screens/index";
 import { Landing } from "./src/screens/Landing/index";
-import { SignUp } from "./src/screens/Landing/SignUp/index";
 
 export default function App() {
   return (
