fcc0ebb6572ca23bde941f49d3f6541a75aa315d
agunderman10 <agunderman10@gmail.com>
2020-10-14 23:01:46 -0400

    Added structure for AddItems screen

---
+++ src/screens/AddItems/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const AddItems = () => {
+    return (
+        <View><Text>Hello there from AddItems</Text></View>
+    )
+}
