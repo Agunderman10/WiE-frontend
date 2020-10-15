170b3356e12800633da5081b44d676a66f2236c8
agunderman10 <agunderman10@gmail.com>
2020-10-15 00:49:02 -0400

    Added remaining categories to the category picker

--- src/screens/AddItems/index.js
+++ src/screens/AddItems/index.js
@@ -35,7 +35,14 @@ export const AddItems = () => {
       <View style={styles.textInputContainer}>
         <Picker style={styles.categoryPicker} selectedValue={selectedCategory} onValueChange={(itemValue, itemIndex) => setSelectedCategory(itemValue)}>
           <Picker.Item label="MEP PREFACE" value="MEP PREFACE" />
+          <Picker.Item label="WiE LC Events" value="WiE LC Events" />
           <Picker.Item label="DOI Events" value="DOI Events" />
+          <Picker.Item label="FE 1181" value="FE 1181" />
+          <Picker.Item label="FEH" value="FEH" />
+          <Picker.Item label="Physics 1250" value="Physics 1250" />
+          <Picker.Item label="Physics 1251" value="Physics 1251" />
+          <Picker.Item label="Chem 1210" value="Chem 1210" />
+          <Picker.Item label="Math 1172" value="Math 1172" />
         </Picker>
       </View>
 
