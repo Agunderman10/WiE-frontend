52f1d91eae340bb92d0fa9fb3a1ea0dbc96b73d8
agunderman10 <agunderman10@gmail.com>
2020-10-15 00:34:45 -0400

    Refactor

--- src/screens/AddItems/styles.js
+++ src/screens/AddItems/styles.js
@@ -1,57 +1,57 @@
 import { Button, StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        width: "100%",
-        height: "100%",
-        backgroundColor: "white",
-    },
-    textInputContainer: {
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    textInput: {
-        borderColor: "black",
-        borderWidth: 1,
-        borderRadius: 10,
-        width: "85%",
-        height: 40,
-    },
-    text: {
-        marginTop: 8,
-        marginLeft: 20,
-        marginBottom: 7,
-        fontSize: 17,
-        fontWeight: "bold",
-        color: "black",
-    },
-    timePicker: {
-        marginTop: 5,
-        borderWidth: 1,
-        borderColor: "black",
-        height: 40,
-        width: 100,
-    },
-    categoryPicker: {
-        marginBottom: 5,
-        borderWidth: 1,
-        borderColor: "black",
-        height: 40,
-        width: 175,
-    },
-    button: {
-        marginTop: 5,
-        backgroundColor: "red",
-        width: 110,
-        height: 40,
-        borderRadius: 10,
-        justifyContent: "center",
-        alignSelf: "center",
-        alignItems: "center",
-    },
-    buttonText: {
-        fontWeight: "bold",
-        color: "white",
-    }
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+  },
+  textInputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: "85%",
+    height: 40,
+  },
+  text: {
+    marginTop: 8,
+    marginLeft: 20,
+    marginBottom: 7,
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "black",
+  },
+  timePicker: {
+    marginTop: 5,
+    borderWidth: 1,
+    borderColor: "black",
+    height: 40,
+    width: 100,
+  },
+  categoryPicker: {
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: "black",
+    height: 40,
+    width: 175,
+  },
+  button: {
+    marginTop: 5,
+    backgroundColor: "red",
+    width: 110,
+    height: 40,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
+  },
 });
