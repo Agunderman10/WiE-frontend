14d6087a7264f749e8aa0ae52f9c7a89e1bdc7b7
agunderman10 <agunderman10@gmail.com>
2020-10-14 22:44:20 -0400

    Added item button on settings page and version footer

--- src/screens/Settings/index.js
+++ src/screens/Settings/index.js
@@ -1,8 +1,16 @@
 import React from "react";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { CURRENT_APP_VERSION } from "./../../constants/app";
 
+import { styles } from "./styles";
 export const Settings = () => {
-    return (
-        <Text>Hello</Text>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.itemContainer}>
+        <Text style={styles.itemText}>Add Additional Items</Text>
+      </TouchableOpacity>
+      <Text style={styles.versionNumber}>Version: {CURRENT_APP_VERSION}</Text>
+    </View>
+  );
+};
---
+++ src/screens/Settings/styles.js
@@ -0,0 +1,26 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+    },
+    itemContainer: {
+        width: "100%",
+        height: 60,
+        borderWidth: 2,
+        borderColor: "grey",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "lightgrey",
+        marginTop: 2,
+    },
+    itemText: {
+        fontSize: 15,
+    },
+    versionNumber: {
+        alignSelf: "center",
+        position: "absolute",
+        bottom: 5,
+    }
+});
