aae357c70699eea12451c17c3b3550a7a26b8ef2
agunderman10 <agunderman10@gmail.com>
2020-10-14 23:53:26 -0400

    Added structure of additems page

--- src/screens/AddItems/index.js
+++ src/screens/AddItems/index.js
@@ -1,8 +1,29 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+import { styles } from "./styles";
 
 export const AddItems = () => {
-    return (
-        <View><Text>Hello there from AddItems</Text></View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Event or Study Group Name:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput style={styles.textInput} />
+      </View>
+      <Text style={styles.text}>Zoom Link:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput style={styles.textInput} />
+      </View>
+      <Text style={styles.text}>Date:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput style={styles.textInput} />
+      </View>
+      <Text style={styles.text}>Time:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput style={styles.textInput} />
+      </View>
+      <Text style={styles.text}>Category:</Text>
+    </View>
+  );
+};
--- src/screens/AddItems/styles.js
+++ src/screens/AddItems/styles.js
@@ -1,5 +1,29 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
+    container: {
+        flex: 1,
+        width: "100%",
+        height: "100%",
+        backgroundColor: "white",
+    },
+    textInputContainer: {
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    textInput: {
+        borderColor: "black",
+        borderWidth: 1,
+        borderRadius: 10,
+        width: "85%",
+        height: 50,
+    },
+    text: {
+        marginTop: 8,
+        marginLeft: 20,
+        marginBottom: 7,
+        fontSize: 17,
+        fontWeight: "bold",
+        color: "black",
+    }
 });
