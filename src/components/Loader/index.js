5bf2efdfdfefe26e1edec6f2affdbfaafbcfa4ff
agunderman10 <agunderman10@gmail.com>
2020-10-16 00:57:08 -0400

    Refactored Loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { ActivityIndicator, View } from "react-native";
 
 export const Loader = () => {
-    const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    return (
-        <View>
-        <ActivityIndicator animating={loading} size="large" color="red" />
-        </View>
-    );
-}
+  return (
+    <View>
+      <ActivityIndicator animating={loading} size="large" color="red" />
+    </View>
+  );
+};
