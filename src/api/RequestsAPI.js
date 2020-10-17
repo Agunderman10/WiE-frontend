200658f7bbcb8895f706a87c152d426d8dfde75c
agunderman10 <agunderman10@gmail.com>
2020-10-16 23:04:48 -0400

    Added logic for post request for requests

--- src/api/RequestsAPI.js
+++ src/api/RequestsAPI.js
@@ -5,6 +5,20 @@ export const getRequests = () => {
 
 }
 
-export const postRequest = () => {
-    
+export const postRequest = async (name, link, date, time, timeIsAmOrPm, selectedCategory) => {
+    return await axios
+    .post(url + "study-group-requests", {
+      name: name,
+      link: link,
+      date: date,
+      time: time,
+      timeIsAmOrPm: timeIsAmOrPm,
+      selectedCategory: selectedCategory
+    })
+    .then((res) => {
+      return res.data;
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 }
