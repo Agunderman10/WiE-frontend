4268bba5a69f36b73df5eeac39663bf4748213b9
agunderman10 <agunderman10@gmail.com>
2020-10-16 22:41:32 -0400

    Added code for add request screen. will need to change some parts

--- src/screens/AddRequest/index.js
+++ src/screens/AddRequest/index.js
@@ -1,10 +1,146 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { View, Text, Button, Alert } from "react-native";
+import { TextInput, TouchableOpacity } from "react-native-gesture-handler";
+import { Picker } from "@react-native-community/picker";
+import { postEvent } from "./../../api/EventsAPI";
+import { postStudyGroup } from "./../../api/StudyGroupsAPI";
+import { Loader } from "./../../components/Loader/index";
 
 import { styles } from "./styles";
 
 export const AddRequest = () => {
-    return (
-        <View><Text>Hello there from Add Request</Text></View>
-    );
-}
+  const [name, setName] = useState("");
+  const [link, setLink] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+  const [timeIsAmOrPm, setTimeIsAmOrPm] = useState("am");
+  const [selectedCategory, setSelectedCategory] = useState("MEP PREFACE");
+  const [loading, setLoading] = useState(false);
+
+  const submitItem = () => {
+    setLoading(true);
+
+    // if selectedCategory is a study group or event
+    if (checkIfAnyInputsAreEmpty()) {
+      setTimeout(() => {
+        Alert.alert(
+          "Error",
+          "One or more required pieces of information are blank. Please fill in all of the text boxes."
+        );
+        setLoading(false);
+      }, 2000);
+    } else if (
+      selectedCategory === "MEP PREFACE" ||
+      selectedCategory === "WiE LC Events" ||
+      selectedCategory === "DOI Events"
+    ) {
+      setTimeout(() => {
+        postEvent(name, link, date, time, timeIsAmOrPm, selectedCategory);
+        clearAllInputs();
+        Alert.alert("Success", "Your new item was added!");
+        setLoading(false);
+      }, 2000);
+    } else {
+      setTimeout(() => {
+        postStudyGroup(name, link, date, time, timeIsAmOrPm, selectedCategory);
+        setLoading(false);
+      }, 2000);
+    }
+    console.log(name, link, date, time, timeIsAmOrPm, selectedCategory);
+  };
+
+  const clearAllInputs = () => {
+    setName("");
+    setLink("");
+    setDate("");
+    setTime("");
+    setTimeIsAmOrPm("am");
+    setSelectedCategory("MEP PREFACE");
+  };
+
+  const checkIfAnyInputsAreEmpty = () => {
+    if (!name.trim() || !link.trim() || !date.trim() || !time.trim()) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Loader isLoading={loading} />
+      <Text style={styles.text}>Event or Study Group Name:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={name}
+          style={styles.textInput}
+          onChangeText={(newValue) => {
+            setName(newValue);
+          }}
+        />
+      </View>
+      <Text style={styles.text}>Zoom Link:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={link}
+          style={styles.textInput}
+          onChangeText={(newValue) => {
+            setLink(newValue);
+          }}
+        />
+      </View>
+      <Text style={styles.text}>Date:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={date}
+          style={styles.textInput}
+          onChangeText={(newValue) => {
+            setDate(newValue);
+          }}
+        />
+      </View>
+      <Text style={styles.text}>Time:</Text>
+      <View style={styles.textInputContainer}>
+        <TextInput
+          value={time}
+          style={styles.textInput}
+          onChangeText={(newValue) => {
+            setTime(newValue);
+          }}
+        />
+        <Picker
+          style={styles.timePicker}
+          selectedValue={timeIsAmOrPm}
+          onValueChange={(itemValue, itemIndex) => setTimeIsAmOrPm(itemValue)}
+        >
+          <Picker.Item label="am" value="am" />
+          <Picker.Item label="pm" value="pm" />
+        </Picker>
+      </View>
+      <Text style={styles.text}>Category:</Text>
+      <View style={styles.textInputContainer}>
+        <Picker
+          style={styles.categoryPicker}
+          selectedValue={selectedCategory}
+          onValueChange={(itemValue, itemIndex) =>
+            setSelectedCategory(itemValue)
+          }
+        >
+          <Picker.Item label="MEP PREFACE" value="MEP PREFACE" />
+          <Picker.Item label="WiE LC Events" value="WiE LC Events" />
+          <Picker.Item label="DOI Events" value="DOI Events" />
+          <Picker.Item label="FE 1181" value="FE 1181" />
+          <Picker.Item label="FEH" value="FEH" />
+          <Picker.Item label="Physics 1250" value="Physics 1250" />
+          <Picker.Item label="Physics 1251" value="Physics 1251" />
+          <Picker.Item label="Chem 1210" value="Chem 1210" />
+          <Picker.Item label="Math 1172" value="Math 1172" />
+        </Picker>
+      </View>
+
+      <TouchableOpacity style={styles.button} onPress={() => submitItem()}>
+        <Text style={styles.buttonText}>Submit Item</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
--- src/screens/AddRequest/styles.js
+++ src/screens/AddRequest/styles.js
@@ -1,5 +1,53 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    width: "100%",
+    height: "100%",
+    backgroundColor: "white",
+  },
+  textInputContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textInput: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderRadius: 10,
+    width: "85%",
+    height: 40,
+  },
+  text: {
+    marginTop: 8,
+    marginLeft: 20,
+    marginBottom: 7,
+    fontSize: 17,
+    fontWeight: "bold",
+    color: "black",
+  },
+  timePicker: {
+    marginTop: 5,
+    height: 40,
+    width: 100,
+  },
+  categoryPicker: {
+    marginBottom: 5,
+    height: 40,
+    width: 175,
+  },
+  button: {
+    marginTop: 5,
+    backgroundColor: "red",
+    width: 110,
+    height: 40,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignSelf: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
+  },
 });
