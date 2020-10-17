8a534a8ea2cb1b8d250376b0a875f0323ba71a95
agunderman10 <agunderman10@gmail.com>
2020-10-16 23:17:05 -0400

    Added pending requests component

---
+++ src/screens/PendingRequests/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export const PendingRequests = () => {
+    return (
+        <View><Text>Hello there from PendingRequests</Text></View>
+    );
+}
