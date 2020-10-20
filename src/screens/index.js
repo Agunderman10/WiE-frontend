33855a0aadcd23c5aa36676bf78bca6831cc7430
agunderman10 <agunderman10@gmail.com>
2020-10-20 19:56:43 -0400

    Refactor

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,15 +1,5 @@
 import * as React from "react";
-
-import { createStackNavigator } from "@react-navigation/stack";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { Feather } from "@expo/vector-icons";
-
-import { Home } from "./Home/index";
-import { Resources } from "./Resources/index";
-import { StudyGroups } from "./StudyGroups/index";
-import { FAQ } from "./FAQ/index";
 import { Landing } from "./Landing";
-import { HomeStack } from "../components/HomeStack";
 import { Navigator } from "../components/Navigator";
 
 export function Screens() {
