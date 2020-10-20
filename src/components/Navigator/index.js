e67057fe4666a4167aa970f3f3a1b6e3106b950d
agunderman10 <agunderman10@gmail.com>
2020-10-20 19:55:52 -0400

    Moved Navigator to its own component

---
+++ src/components/Navigator/index.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Resources } from "./../../screens/Resources/index";
+import { StudyGroups } from "./../../screens/StudyGroups/index";
+import { FAQ } from "./../../screens/FAQ";
+import { HomeStack } from "./../HomeStack/index";
+
+const Tab = createBottomTabNavigator();
+
+
+export function Navigator() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Home"
+      tabBarOptions={{
+        activeTintColor: "red",
+      }}
+    >
+      <Tab.Screen
+        name="Home"
+        children={HomeStack}
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Resources"
+        component={Resources}
+        options={{
+          tabBarLabel: "Resources",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="account-group"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Study Groups"
+        component={StudyGroups}
+        options={({ navigation }) => ({
+          tabBarLabel: "Study Groups",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="book-open-page-variant"
+              color={color}
+              size={size}
+            />
+          ),
+        })}
+      />
+      <Tab.Screen
+        name="FAQ"
+        component={FAQ}
+        options={{
+          tabBarLabel: "FAQ",
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons
+              name="frequently-asked-questions"
+              color={color}
+              size={size}
+            />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
 import { createStackNavigator } from "@react-navigation/stack";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
@@ -10,75 +10,10 @@ import { StudyGroups } from "./StudyGroups/index";
 import { FAQ } from "./FAQ/index";
 import { Landing } from "./Landing";
 import { HomeStack } from "../components/HomeStack";
-
-const Tab = createBottomTabNavigator();
-
-function Navigator() {
-  return (
-    <Tab.Navigator
-      initialRouteName="Home"
-      tabBarOptions={{
-        activeTintColor: "red",
-      }}
-    >
-      <Tab.Screen
-        name="Home"
-        children={HomeStack}
-        options={{
-          tabBarLabel: "Home",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="home" color={color} size={size} />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Resources"
-        component={Resources}
-        options={{
-          tabBarLabel: "Resources",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="account-group"
-              color={color}
-              size={size}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Study Groups"
-        component={StudyGroups}
-        options={({ navigation }) => ({
-          tabBarLabel: "Study Groups",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="book-open-page-variant"
-              color={color}
-              size={size}
-            />
-          ),
-        })}
-      />
-      <Tab.Screen
-        name="FAQ"
-        component={FAQ}
-        options={{
-          tabBarLabel: "FAQ",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons
-              name="frequently-asked-questions"
-              color={color}
-              size={size}
-            />
-          ),
-        }}
-      />
-    </Tab.Navigator>
-  );
-}
+import { Navigator } from "../components/Navigator";
 
 export function Screens() {
-  let isSignedIn = true;
+  let isSignedIn = false;
 
   return (
     isSignedIn ? (
