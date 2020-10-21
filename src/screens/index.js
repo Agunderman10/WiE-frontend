ebc1ab38a6c721ce6ee8e31dc8b12e0abb89ae94
agunderman10 <agunderman10@gmail.com>
2020-10-20 20:24:33 -0400

    Added new api calls in the Screens component

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,15 +1,32 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { Landing } from "./Landing";
 import { Navigator } from "../components/Navigator";
+import { getEvents } from "../api/EventsAPI";
+import { getStudyGroups } from "../api/StudyGroupsAPI";
 
 export function Screens() {
-  let isSignedIn = false;
+  const [events, setEvents] = useState();
+  const [studyGroups, setStudyGroups] = useState();
 
-  return (
-    isSignedIn ? (
-      <Navigator />
-    ) : (
-      <Landing />
-    )
-  );
+  let isSignedIn = true;
+  // get login info from keychain. if logged in display navigator, if not display landing
+
+  useEffect(() => {
+    getEventsFromAPI();
+    getStudyGroupsFromAPI();
+  }, []);
+
+  const getEventsFromAPI = async () => {
+    await getEvents().then((data) => {
+      setEvents(data);
+    });
+  };
+
+  const getStudyGroupsFromAPI = async () => {
+    await getStudyGroups().then((data) => {
+      setStudyGroups(data);
+    });
+  };
+
+  return isSignedIn ? <Navigator eventsData={events} studyGroupsData={studyGroups} /> : <Landing />;
 }
