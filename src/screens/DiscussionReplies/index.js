97f69b147ec1bde788a162ad620378d59e04aea4
agunderman10 <agunderman10@gmail.com>
2020-12-03 16:27:31 -0500

    Added ChosenDiscussion component and imported it

--- src/screens/DiscussionReplies/index.js
+++ src/screens/DiscussionReplies/index.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { DiscussionCard } from "../../components/DiscussionCard";
+import { ChosenDiscussion } from "./components/ChosenDiscussion";
 
 export const DiscussionReplies = () => {
-    return (
-        <View>
-            <Text>Hello there</Text>
-        </View>
-    )
-}
+  return (
+    <View>
+      <ChosenDiscussion />
+      <DiscussionCard
+        name="Andrew Gunderman"
+        timePosted="23 minutes ago"
+        message="Does anyone know where I can find a cheaper version of our Physics 1250 book?"
+      />
+      <DiscussionCard
+        name="Andrew Gunderman"
+        timePosted="23 minutes ago"
+        message="Does anyone know where I can find a cheaper version of our Physics 1250 book?"
+      />
+    </View>
+  );
+};
