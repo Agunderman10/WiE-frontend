79663f6e87e13cc02a68a67dc3f4148c1620b33c
agunderman10 <agunderman10@gmail.com>
2020-12-03 13:59:18 -0500

    Added Discussion Replies component

---
+++ src/screens/DiscussionReplies/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const DiscussionReplies = () => {
+    return (
+        <View>
+            <Text>Hello there</Text>
+        </View>
+    )
+}
