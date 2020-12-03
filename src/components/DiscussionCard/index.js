e5d16b2d111d68ebf1d3663126a2f5a680bb1be6
agunderman10 <agunderman10@gmail.com>
2020-12-03 14:54:03 -0500

    Added navigation to individual discussions

--- src/components/DiscussionCard/index.js
+++ src/components/DiscussionCard/index.js
@@ -1,10 +1,12 @@
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export const DiscussionCard = ({ name, timePosted, message}) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity style={styles.discussionContainer}>
+    <TouchableOpacity style={styles.discussionContainer} onPress={() => navigation.navigate("Discussion")}>
         <Text style={styles.userName}>{name}</Text>
         <Text style={styles.timePosted}>{timePosted}</Text>
         <Text style={styles.message}>{message}</Text>
